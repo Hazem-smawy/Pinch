0d9ddb27ee7e527d8e869af27da7e9b22d3b1630
hazem-smawy <hazemsmawy@gmail.com>
2022-09-01 22:11:32 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by hazem smawy on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
